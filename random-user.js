ceffee6fd801c739be8adef8e8df807515b75fc1
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-23 20:00:26 +0600

    add dynamic style

--- random-user.js
+++ random-user.js
@@ -1,7 +1,20 @@
 const randomUser = () => {
     fetch('https://randomuser.me/api/?results=5')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayRead(data))
 
 }
 randomUser()
+
+const displayRead = (user) => {
+    const buddyes = user.results;
+    const buddyesDiv = document.getElementById('random-info');
+    for (const buddye of buddyes) {
+        console.log(buddye);
+        const p = document.createElement('p');
+        p.innerText = ` Name: ${buddye.name.title} ${buddye.name.first} ${buddye.name.last} || E-mail: ${buddye.email} || Phone: ${buddye.cell}
+        `;
+        buddyesDiv.appendChild(p);
+
+    }
+}
