c1bd6a59bb8020b4dcdfc3785b1d75bf17754d8d
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-24 20:54:03 +0600

    add card done

--- mealdb/mealdb.js
+++ mealdb/mealdb.js
@@ -5,10 +5,23 @@ const loadFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValu}`
     fetch(url)
         .then(search => search.json())
-        .then(data => displayInput(data.meals))
+        .then(data => displayResult(data.meals))
 }
-const displayInput = meals => {
-    console.log(meals);
+const displayResult = result => {
     const searchResult = document.getElementById('search-result');
-    const
+    result.forEach(values => {
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
